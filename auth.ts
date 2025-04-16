613933d78748efbe716eceff1b086f7ea6396d40
bugrabasbostanci <bb0143sbw+github@gmail.com>
2025-04-17 02:02:49 +0300

    feat: update authentication configuration and environment variables
    - Add authorization parameters for Google sign-in in auth.ts to enhance user consent flow.
    - Introduce AUTH_URL environment variable in next.config.ts for better configuration management.

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: { params: { prompt: "consent", access_type: "offline", response_type: "code" } }
     }),
   ],
   pages: {
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    AUTH_URL: process.env.AUTH_URL,
+  }
 };
 
 export default nextConfig;
